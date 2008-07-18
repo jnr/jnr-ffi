398cd14ac5815b9462b8c9e09e4eacf85f5ecc76
Wayne Meissner <wmeissner@gmail.com>
2008-07-18 14:58:55 +1000

    Add more high level functionality from jffi

--- src/jafl/FFIProvider.java
+++ src/jafl/FFIProvider.java
@@ -1,9 +1,24 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2007, 2008 Wayne Meissner
+ *
+ * This file is part of jffi.
+ *
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * version 3 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package jafl;
+
+import java.util.Map;
 
 /**
  *
@@ -15,4 +30,17 @@ public abstract class FFIProvider {
     }
     public abstract MemoryIO allocateMemory(int size);
     public abstract MemoryIO allocateMemoryDirect(int size);
+    /**
+     * Loads a native library and links the methods defined in {@code interfaceClass}
+     * to native methods in the library.
+     *
+     * @param libraryName the name of the library to load
+     * @param interfaceClass the interface that describes the native library interface
+     * @param libraryOptions options
+     * @return an instance of {@code interfaceclass} that will call the native methods.
+     */
+    public abstract <T> T loadLibrary(String libraryName, Class<T> interfaceClass,
+            Map<LibraryOption, ?> libraryOptions);
+    public abstract int getLastError();
+    public abstract void setLastError(int error);
 }
