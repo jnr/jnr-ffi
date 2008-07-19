25f5248c3bc51de7294e347eebd11fd99cacf80e
Wayne Meissner <wmeissner@gmail.com>
2008-07-19 18:33:48 +1000

    Add some javadoc to FFIProvider

--- src/jafl/FFIProvider.java
+++ src/jafl/FFIProvider.java
@@ -25,8 +25,13 @@ import java.util.Map;
  * @author wayne
  */
 public abstract class FFIProvider {
+    /**
+     * Gets an instance of <tt>FFIProvider</tt>
+     *
+     * @return an instance of <tt>FFIProvider</tt>
+     */
     public static final FFIProvider getInstance() {
-        throw new UnsupportedOperationException("not implemented");
+        return SingletonHolder.INSTANCE;
     }
     public abstract MemoryIO allocateMemory(int size);
     public abstract MemoryIO allocateMemoryDirect(int size);
@@ -41,6 +46,45 @@ public abstract class FFIProvider {
      */
     public abstract <T> T loadLibrary(String libraryName, Class<T> interfaceClass,
             Map<LibraryOption, ?> libraryOptions);
+
+    /**
+     * Gets the last native error code.
+     * <p>
+     * This returns the errno value that was set at the time of the last native
+     * function call.
+     *
+     * @return The errno value.
+     */
     public abstract int getLastError();
+
+    /**
+     * Sets the native error code.
+     *
+     * @param error The value to set errno to.
+     */
     public abstract void setLastError(int error);
+
+    private static final class SingletonHolder {
+        private static final FFIProvider INSTANCE = getInstance();
+        private static final FFIProvider getInstance() {
+            final boolean useJNA = Boolean.getBoolean("jafl.UseJNA");
+            FFIProvider provider = null;
+            String prefix = FFIProvider.class.getPackage().getName();
+            if (!useJNA) {
+                try {
+                    provider = (FFIProvider) Class.forName(prefix + ".jffi.JFFIProvider").newInstance();
+                } catch (Throwable ex) {
+                }
+            }
+            if (provider == null) {
+                try {
+                    provider = (FFIProvider) Class.forName(prefix + ".jna.JNAProvider").newInstance();
+                } catch (Throwable ex) {
+                    throw new RuntimeException("Could not load FFI provider", ex);
+                }
+            }
+            return provider;
+        }
+    }
+    protected FFIProvider() {}
 }
