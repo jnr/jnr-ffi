317b777cdaa2ea0bd3c2ef0a934a442304b3b749
Wayne Meissner <wmeissner@gmail.com>
2009-01-03 18:57:59 +1000

    Make everything final

--- src/com/kenai/jaffl/provider/NullMemoryIO.java
+++ src/com/kenai/jaffl/provider/NullMemoryIO.java
@@ -1,147 +1,140 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.kenai.jaffl.provider;
 
 import com.kenai.jaffl.MemoryIO;
 import com.kenai.jaffl.Pointer;
 
-/**
- *
- * @author wayne
- */
-public class NullMemoryIO extends AbstractMemoryIO {
+public final class NullMemoryIO extends AbstractMemoryIO {
     private static final String msg = "Attempted access to a NULL memory address";
 
     public static final MemoryIO INSTANCE = new NullMemoryIO();
 
-    private final NullPointerException npe() {
+    private static final NullPointerException npe() {
         return new NullPointerException(msg);
     }
 
-    public byte getByte(long offset) {
+    public final byte getByte(long offset) {
         throw npe();
     }
 
-    public short getShort(long offset) {
+    public final short getShort(long offset) {
         throw npe();
     }
 
-    public int getInt(long offset) {
+    public final int getInt(long offset) {
         throw npe();
     }
 
-    public long getLong(long offset) {
+    public final long getLong(long offset) {
         throw npe();
     }
 
-    public float getFloat(long offset) {
+    public final float getFloat(long offset) {
         throw npe();
     }
 
-    public double getDouble(long offset) {
+    public final double getDouble(long offset) {
         throw npe();
     }
 
-    public void putByte(long offset, byte value) {
+    public final void putByte(long offset, byte value) {
         throw npe();
     }
 
-    public void putShort(long offset, short value) {
+    public final void putShort(long offset, short value) {
         throw npe();
     }
 
-    public void putInt(long offset, int value) {
+    public final void putInt(long offset, int value) {
         throw npe();
     }
 
-    public void putLong(long offset, long value) {
+    public final void putLong(long offset, long value) {
         throw npe();
     }
 
-    public void putFloat(long offset, float value) {
+    public final void putFloat(long offset, float value) {
         throw npe();
     }
 
-    public void putDouble(long offset, double value) {
+    public final void putDouble(long offset, double value) {
         throw npe();
     }
 
-    public void get(long offset, byte[] dst, int off, int len) {
+    public final void get(long offset, byte[] dst, int off, int len) {
         throw npe();
     }
 
-    public void put(long offset, byte[] dst, int off, int len) {
+    public final void put(long offset, byte[] dst, int off, int len) {
         throw npe();
     }
 
-    public void get(long offset, short[] dst, int off, int len) {
+    public final void get(long offset, short[] dst, int off, int len) {
         throw npe();
     }
 
-    public void put(long offset, short[] dst, int off, int len) {
+    public final void put(long offset, short[] dst, int off, int len) {
         throw npe();
     }
 
-    public void get(long offset, int[] dst, int off, int len) {
+    public final void get(long offset, int[] dst, int off, int len) {
         throw npe();
     }
 
-    public void put(long offset, int[] dst, int off, int len) {
+    public final void put(long offset, int[] dst, int off, int len) {
         throw npe();
     }
 
-    public void get(long offset, long[] dst, int off, int len) {
+    public final void get(long offset, long[] dst, int off, int len) {
         throw npe();
     }
 
-    public void put(long offset, long[] dst, int off, int len) {
+    public final void put(long offset, long[] dst, int off, int len) {
         throw npe();
     }
 
-    public void get(long offset, float[] dst, int off, int len) {
+    public final void get(long offset, float[] dst, int off, int len) {
         throw npe();
     }
 
-    public void put(long offset, float[] dst, int off, int len) {
+    public final void put(long offset, float[] dst, int off, int len) {
         throw npe();
     }
 
-    public void get(long offset, double[] dst, int off, int len) {
+    public final void get(long offset, double[] dst, int off, int len) {
         throw npe();
     }
 
-    public void put(long offset, double[] dst, int off, int len) {
+    public final void put(long offset, double[] dst, int off, int len) {
         throw npe();
     }
 
-    public MemoryIO getMemoryIO(long offset) {
+    public final MemoryIO getMemoryIO(long offset) {
         throw npe();
     }
 
-    public MemoryIO getMemoryIO(long offset, long size) {
+    public final MemoryIO getMemoryIO(long offset, long size) {
         throw npe();
     }
 
-    public Pointer getPointer(long offset) {
+    public final Pointer getPointer(long offset) {
         throw npe();
     }
 
-    public void putPointer(long offset, Pointer value) {
+    public final void putPointer(long offset, Pointer value) {
         throw npe();
     }
 
-    public int indexOf(long offset, byte value, int maxlen) {
+    public final int indexOf(long offset, byte value, int maxlen) {
         throw npe();
     }
 
-    public boolean isDirect() {
-        throw npe();
+    public final boolean isDirect() {
+        return true;
     }
 
     @Override
-    public void setMemory(long offset, long size, byte value) {
+    public final void setMemory(long offset, long size, byte value) {
         throw npe();
     }
 }
