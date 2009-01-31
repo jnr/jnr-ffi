867ab123f27a3058eb6ff1a691085ea3dcb3afe5
Wayne Meissner <wmeissner@gmail.com>
2009-01-31 22:16:48 +1000

    Remove superfluous comment

--- src/com/kenai/jaffl/mapper/TypeMapper.java
+++ src/com/kenai/jaffl/mapper/TypeMapper.java
@@ -1,10 +1,6 @@
 
 package com.kenai.jaffl.mapper;
 
-/**
- *
- * @author wayne
- */
 public interface TypeMapper {
     public FromNativeConverter getFromNativeConverter(Class type);
     public ToNativeConverter getToNativeConverter(Class type);
