4184e6ea35e54eeb38bbec925f48e213b82725e6
Wayne Meissner <wmeissner@gmail.com>
2009-05-16 13:31:15 +1000

    Remove superfluous comments

--- src/com/kenai/jaffl/mapper/ToNativeContext.java
+++ src/com/kenai/jaffl/mapper/ToNativeContext.java
@@ -1,10 +1,6 @@
 
 package com.kenai.jaffl.mapper;
 
-/**
- *
- * @author wayne
- */
 public interface ToNativeContext {
 
 }
--- src/com/kenai/jaffl/mapper/ToNativeConverter.java
+++ src/com/kenai/jaffl/mapper/ToNativeConverter.java
@@ -1,10 +1,6 @@
 
 package com.kenai.jaffl.mapper;
 
-/**
- *
- * @author wayne
- */
 public interface ToNativeConverter<J, N> {
     public N toNative(J value, ToNativeContext context);
 }
