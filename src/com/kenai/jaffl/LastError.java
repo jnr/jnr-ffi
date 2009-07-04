0a310ecda5a249caa355db91696b388651770e58
Wayne Meissner <wmeissner@gmail.com>
2009-07-04 21:16:08 +1000

    Add javadoc

--- src/com/kenai/jaffl/LastError.java
+++ src/com/kenai/jaffl/LastError.java
@@ -2,13 +2,25 @@
 package com.kenai.jaffl;
 
 /**
- * Enables getting/setting of the unix errno value
+ * Enables getting/setting of the unix errno and windows GetLastError() value
  */
 public final class LastError {
     private LastError() {}
+
+    /**
+     * Gets the value of errno from the last native call.
+     *
+     * @return An integer containing the errno value.
+     */
     public static final int getLastError() {
         return FFIProvider.getProvider().getLastError();
     }
+
+    /**
+     * Sets the native errno value.
+     *
+     * @param error The value to set errno to.
+     */
     public static final void setLastError(int error) {
         FFIProvider.getProvider().setLastError(error);
     }
