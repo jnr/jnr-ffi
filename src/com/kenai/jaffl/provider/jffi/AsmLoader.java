f96eae865da1688fa71ce2c47370009ba8a68d18
Wayne Meissner <wmeissner@gmail.com>
2009-11-06 19:28:17 +1000

    Use the ClassLoader of the interface as the parent of the Asm class loader

--- src/com/kenai/jaffl/provider/jffi/AsmLoader.java
+++ src/com/kenai/jaffl/provider/jffi/AsmLoader.java
@@ -4,6 +4,14 @@ final class AsmLoader extends ClassLoader {
 
     static final AsmLoader INSTANCE = new AsmLoader();
 
+    public AsmLoader() {
+    }
+
+    public AsmLoader(ClassLoader parent) {
+        super(parent);
+    }
+
+
     public Class defineClass(String name, byte[] b) {
         return defineClass(name, b, 0, b.length);
     }
