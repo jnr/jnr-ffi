f42e76845ab869aab78f54e843134f0650c58afe
Wayne Meissner <wmeissner@gmail.com>
2010-01-08 10:18:10 +1000

    Add some generated function indexes for JNIEnv and JavaVM

---
+++ src/com/kenai/jaffl/provider/jffi/JNIInvokeInterface.java
@@ -0,0 +1,14 @@
+
+
+package com.kenai.jaffl.provider.jffi;
+
+/**
+ * Indexes of JavaVM methods
+ */
+public class JNIInvokeInterface {
+    public static final int DestroyJavaVM = 3;
+    public static final int AttachCurrentThread = 4;
+    public static final int DetachCurrentThread = 5;
+    public static final int GetEnv = 6;
+    public static final int AttachCurrentThreadAsDaemon = 7;
+}
