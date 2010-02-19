c6afeca60d65c794d4936bfcd6222d222a439538
Wayne Meissner <wmeissner@gmail.com>
2010-02-20 01:53:55 +1000

    Extract the BadType class so it can be used by multiple providers

---
+++ src/com/kenai/jaffl/provider/BadType.java
@@ -0,0 +1,21 @@
+package com.kenai.jaffl.provider;
+
+/**
+ *
+ */
+public final class BadType implements com.kenai.jaffl.Type {
+
+    private final NativeType nativeType;
+
+    public BadType(NativeType type) {
+        this.nativeType = type;
+    }
+
+    public final int alignment() {
+        throw new RuntimeException("invalid type: " + nativeType);
+    }
+
+    public final int size() {
+        throw new RuntimeException("invalid type: " + nativeType);
+    }
+}
