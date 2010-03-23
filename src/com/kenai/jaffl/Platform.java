50d5fa4787634fa6777ccc888aec004279e55689
Wayne Meissner <wmeissner@gmail.com>
2010-03-23 19:05:02 +1000

    Move CPU and OS back into Platform for backwards compat with older jaffl releases

--- src/com/kenai/jaffl/Platform.java
+++ src/com/kenai/jaffl/Platform.java
@@ -18,6 +18,92 @@ public abstract class Platform {
     }
 
     /**
+     * The common names of supported operating systems.
+     */
+    public enum OS {
+        /*
+         * Note The names of the enum values are used in other parts of the
+         * code to determine where to find the native stub library.  Do not rename.
+         */
+
+        /** MacOSX */
+        DARWIN,
+        /** FreeBSD */
+        FREEBSD,
+        /** NetBSD */
+        NETBSD,
+        /** OpenBSD */
+        OPENBSD,
+        /** Linux */
+        LINUX,
+        /** Solaris (and OpenSolaris) */
+        SOLARIS,
+        /** The evil borg operating system */
+        WINDOWS,
+        /** IBM AIX */
+        AIX,
+        /** IBM zOS **/
+        ZLINUX,
+        /** No idea what the operating system is */
+        UNKNOWN;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+
+        /**
+     * The supported CPU architectures.
+     */
+    public enum CPU {
+        /*
+         * <b>Note</b> The names of the enum values are used in other parts of the
+         * code to determine where to find the native stub library.  Do NOT rename.
+         */
+
+        /** 32 bit legacy Intel */
+        I386,
+
+        /** 64 bit AMD (aka EM64T/X64) */
+        X86_64,
+
+        /** 32 bit Power PC */
+        PPC,
+
+        /** 64 bit Power PC */
+        PPC64,
+
+        /** 32 bit Sun sparc */
+        SPARC,
+
+        /** 64 bit Sun sparc */
+        SPARCV9,
+
+        /** IBM zSeries S/390 */
+        S390X,
+
+        /** 32 bit MIPS (used by nestedvm) */
+        MIPS32,
+
+        /**
+         * Unknown CPU architecture.  A best effort will be made to infer architecture
+         * specific values such as address and long size.
+         */
+        UNKNOWN;
+
+        /**
+         * Returns a {@code String} object representing this {@code CPU} object.
+         *
+         * @return the name of the cpu architecture as a lower case {@code String}.
+         */
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+
+    /**
      * Determines the operating system jffi is running on
      *
      * @return An member of the <tt>OS</tt> enum.
@@ -144,6 +230,11 @@ public abstract class Platform {
      * @return The current platform.
      */
     public static final Platform getNativePlatform() {
+        return SingletonHolder.PLATFORM;
+    }
+
+    @Deprecated
+    public static final Platform getPlatform() {
         return SingletonHolder.PLATFORM;
     }
 
