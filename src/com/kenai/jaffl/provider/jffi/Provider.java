58b9020299933a2cce426c4eca47468d5751eb36
Wayne Meissner <wmeissner@gmail.com>
2010-05-03 10:33:06 +1000

    Allow asm compilation to be turned off if jaffl.compile.enabled=false

--- src/com/kenai/jaffl/provider/jffi/Provider.java
+++ src/com/kenai/jaffl/provider/jffi/Provider.java
@@ -28,8 +28,10 @@ public final class Provider extends com.kenai.jaffl.FFIProvider {
     }
 
     private <T> T loadLibrary(NativeLibrary library, Class<T> interfaceClass, Map<LibraryOption, ?> libraryOptions) {
+        final boolean compile = Boolean.parseBoolean(System.getProperty("jaffl.compile.enabled", "true"));
+
         try {
-            if (AsmLibraryLoader.getInstance().isInterfaceSupported(interfaceClass, libraryOptions)) {
+            if (compile && AsmLibraryLoader.getInstance().isInterfaceSupported(interfaceClass, libraryOptions)) {
                 return AsmLibraryLoader.getInstance().loadLibrary(library, interfaceClass, libraryOptions);
             }
         } catch (Exception ex) {
