dc73d1cb13ea99fa726132fce823599169dbb2ec
Wayne Meissner <wmeissner@gmail.com>
2010-11-05 15:08:28 +1000

    Rework StringIO thread locals to keep only the encoder/decoder in the thread local, so there are no references to the StringIO class itself.

--- src/com/kenai/jaffl/provider/StringIO.java
+++ src/com/kenai/jaffl/provider/StringIO.java
@@ -14,47 +14,83 @@ import java.nio.charset.CodingErrorAction;
 /**
  *
  */
-public class StringIO {
-    private static final class SingletonHolder {
+public final class StringIO {
 
-        public static final ThreadLocal<SoftReference<StringIO>> THREAD_LOCAL_STRINGIO = new ThreadLocal<SoftReference<StringIO>>() {
-
-            @Override
-            protected synchronized SoftReference<StringIO> initialValue() {
-                return new SoftReference<StringIO>(new StringIO());
-            }
-        };
+    private static final class StaticDataHolder {
+        static final StringIO INSTANCE = new StringIO(Charset.defaultCharset());
+        static final ThreadLocal<SoftReference<CharsetEncoder>> ENCODER = new ThreadLocal<SoftReference<CharsetEncoder>>();
+        static final ThreadLocal<SoftReference<CharsetDecoder>> DECODER = new ThreadLocal<SoftReference<CharsetDecoder>>();
     }
 
-    public static final StringIO getStringIO() {
-        StringIO io = SingletonHolder.THREAD_LOCAL_STRINGIO.get().get();
-        if (io == null) {
-            SingletonHolder.THREAD_LOCAL_STRINGIO.set(new SoftReference<StringIO>(io = new StringIO()));
+    private final Charset charset;
+    
+    public static StringIO getStringIO() {
+        return StaticDataHolder.INSTANCE;
+    }
+
+    public static StringIO getStringIO(Charset charset) {
+        return new StringIO(charset);
+    }
+    
+    private StringIO(Charset charset) {
+        this.charset = charset;
+    }
+
+    private static CharsetEncoder getEncoder(Charset charset) {
+        SoftReference<CharsetEncoder> ref = StaticDataHolder.ENCODER.get();
+        CharsetEncoder encoder;
+        if (ref != null && (encoder = ref.get()) != null && encoder.charset() == charset) {
+            return encoder;
         }
-        return io;
-    }
-    
-    public final CharsetEncoder encoder = Charset.defaultCharset().newEncoder();
-    public final CharsetDecoder decoder = Charset.defaultCharset().newDecoder();
-    public final int nulByteCount = Math.round(encoder.maxBytesPerChar());
 
-    public StringIO() {
+        return initEncoder(charset);
+    }
+
+    private static CharsetDecoder getDecoder(Charset charset) {
+        SoftReference<CharsetDecoder> ref = StaticDataHolder.DECODER.get();
+        CharsetDecoder decoder;
+        if (ref != null && (decoder = ref.get()) != null && decoder.charset() == charset) {
+            return decoder;
+        }
+
+        return initDecoder(charset);
+    }
+
+    private static CharsetEncoder initEncoder(Charset charset) {
+        CharsetEncoder encoder = charset.newEncoder();
         encoder.onMalformedInput(CodingErrorAction.REPLACE).onUnmappableCharacter(CodingErrorAction.REPLACE);
-        decoder.onMalformedInput(CodingErrorAction.REPLACE).onUnmappableCharacter(CodingErrorAction.REPLACE);
+        StaticDataHolder.ENCODER.set(new SoftReference<CharsetEncoder>(encoder));
+
+        return encoder;
     }
     
+    private static CharsetDecoder initDecoder(Charset charset) {
+        CharsetDecoder decoder = charset.newDecoder();
+        decoder.onMalformedInput(CodingErrorAction.REPLACE).onUnmappableCharacter(CodingErrorAction.REPLACE);
+        StaticDataHolder.DECODER.set(new SoftReference<CharsetDecoder>(decoder));
+
+        return decoder;
+    }
+
     public final ByteBuffer toNative(final CharSequence value, final int minSize, boolean copyIn) {
+        return toNative(getEncoder(charset), value, minSize, copyIn);
+    }
+
+    public final ByteBuffer toNative(final CharSequence value, final ByteBuffer buf) {
+        return toNative(getEncoder(charset), value, buf);
+    }
+
+    private static ByteBuffer toNative(CharsetEncoder encoder, final CharSequence value, final int minSize, boolean copyIn) {
         // Calculate the raw byte size required (with allowance for NUL termination)
         final int len = (int) (((float)Math.max(minSize, value.length()) + 1) * encoder.maxBytesPerChar());
         final ByteBuffer buf = ByteBuffer.allocate(len);
         if (copyIn) {
-            toNative(value, buf);
+            toNative(encoder, value, buf);
         }
         return buf;
     }
 
-    
-    public final ByteBuffer toNative(final CharSequence value, final ByteBuffer buf) {
+    private static ByteBuffer toNative(CharsetEncoder encoder, final CharSequence value, final ByteBuffer buf) {
         //
         // Copy the string to native memory
         //
@@ -63,7 +99,7 @@ public class StringIO {
             encoder.reset();
             encoder.encode(CharBuffer.wrap(value), buf, true);
             encoder.flush(buf);
-            nulTerminate(buf);
+            nulTerminate(encoder, buf);
         } finally {
             buf.reset();
         }
@@ -82,7 +118,7 @@ public class StringIO {
         final int limit = buf.limit();
         buf.limit(end);
         try {
-            return decoder.reset().decode(buf);
+            return getDecoder(charset).reset().decode(buf);
         } catch (CharacterCodingException ex) {
             throw new Error("Illegal character data in native string", ex);
         } finally {
@@ -93,7 +129,7 @@ public class StringIO {
 
     public final CharSequence fromNative(final ByteBuffer buf) {
         try {
-            return decoder.reset().decode(buf);
+            return getDecoder(charset).reset().decode(buf);
         } catch (CharacterCodingException ex) {
             throw new Error("Illegal character data in native string", ex);
         }
@@ -101,8 +137,12 @@ public class StringIO {
 
     
     public final void nulTerminate(ByteBuffer buf) {
+        nulTerminate(getEncoder(charset), buf);
+    }
+
+    private static void nulTerminate(CharsetEncoder encoder, ByteBuffer buf) {
         // NUL terminate the string
-        int nulSize = nulByteCount;
+        int nulSize = Math.round(encoder.maxBytesPerChar());
         while (nulSize >= 4) {
             buf.putInt(0);
             nulSize -= 4;
