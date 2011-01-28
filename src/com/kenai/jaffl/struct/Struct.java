d23c0f591b17cf383f72c6f9fa72a303bd6a60e8
Wayne Meissner <wmeissner@gmail.com>
2011-01-28 19:56:55 +1000

    Switch to dual Apache-2.0/LGPL-3+ licensing, and remove copyright headers from individual files.

--- src/com/kenai/jaffl/struct/Struct.java
+++ src/com/kenai/jaffl/struct/Struct.java
@@ -1,5 +1,7 @@
 /*
- * Some of the design and code of this class is from the javolution project.
+ * This file is covered by the license described in the LICENSE file in the root of
+ * the project, however, it incorporates some code from the javolution project,
+ * and that copyright is reproduced here.
  *
  * Copyright (C) 2006 - Javolution (http://javolution.org/)
  * All rights reserved.
