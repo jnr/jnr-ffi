0783c492a8b18c9f25e4474dba1a0fc4379ff408
Wayne Meissner <wmeissner@gmail.com>
2012-03-14 08:25:03 +1000

    Add some generated type aliases

---
+++ src/main/java/jnr/ffi/annotations/TypeDefinition.java
@@ -0,0 +1,15 @@
+package jnr.ffi.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This is used internally by jnr-ffi to define type aliases.  e.g. ssize_t => long
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { ElementType.TYPE })
+public @interface TypeDefinition {
+    String name();
+}
---
+++ src/main/java/jnr/ffi/types/daddr_t.java
@@ -0,0 +1,16 @@
+  
+package jnr.ffi.types;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jnr.ffi.annotations.TypeDefinition;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { ElementType.PARAMETER, ElementType.METHOD })
+@TypeDefinition(name = "daddr_t")
+public @interface daddr_t {
+
+}
