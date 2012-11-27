70a31199a59186a8775972037a9480a058d99f5f
Wayne Meissner <wmeissner@gmail.com>
2012-11-27 21:41:13 +1100

    Add a caching type mapper

---
+++ src/main/java/jnr/ffi/mapper/CachingTypeMapper.java
@@ -0,0 +1,65 @@
+package jnr.ffi.mapper;
+
+import java.lang.annotation.Annotation;
+import java.util.*;
+
+/**
+ * Caches Class -> native converter lookups.
+ */
+public final class CachingTypeMapper implements TypeMapper {
+    private final TypeMapper mapper;
+    private Map<Class, Map<Collection<Annotation>, ToNativeConverter>> toNativeConverterMap = Collections.emptyMap();
+    private Map<Class, Map<Collection<Annotation>, FromNativeConverter>> fromNativeConverterMap = Collections.emptyMap();
+
+    public CachingTypeMapper(TypeMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public final FromNativeConverter getFromNativeConverter(Class klazz, FromNativeContext context) {
+        Map<Collection<Annotation>, FromNativeConverter> map = fromNativeConverterMap.get(klazz);
+        FromNativeConverter fromNativeConverter = map != null ? map.get(context.getAnnotations()) : null;
+        return fromNativeConverter != null ? fromNativeConverter : lookupAndCacheFromNativeConverter(klazz, context);
+    }
+
+    public final ToNativeConverter getToNativeConverter(Class klazz, ToNativeContext context) {
+        Map<Collection<Annotation>, ToNativeConverter> map = toNativeConverterMap.get(klazz);
+        ToNativeConverter toNativeConverter = map != null ? map.get(context.getAnnotations()) : null;
+        return toNativeConverter != null ? toNativeConverter : lookupAndCacheToNativeConverter(klazz, context);
+    }
+
+    private synchronized FromNativeConverter lookupAndCacheFromNativeConverter(Class klazz, FromNativeContext context) {
+        Map<Collection<Annotation>, FromNativeConverter> converterMap = fromNativeConverterMap.get(klazz);
+        FromNativeConverter fromNativeConverter = converterMap != null ? converterMap.get(context.getAnnotations()) : null;
+        if (fromNativeConverter == null) {
+            Map<Collection<Annotation>, FromNativeConverter> cm = new HashMap<Collection<Annotation>, FromNativeConverter>((converterMap != null ? converterMap.size() : 0) + 1, 0.5f);
+            if (converterMap != null) cm.putAll(converterMap);
+            cm.put(context.getAnnotations(), fromNativeConverter = mapper.getFromNativeConverter(klazz, context));
+
+            Map<Class, Map<Collection<Annotation>, FromNativeConverter>> m
+                    = new IdentityHashMap<Class, Map<Collection<Annotation>, FromNativeConverter>>(fromNativeConverterMap.size() + 1);
+            m.putAll(fromNativeConverterMap);
+            m.put(klazz, Collections.unmodifiableMap(cm));
+            fromNativeConverterMap = Collections.unmodifiableMap(m);
+        }
+
+        return fromNativeConverter;
+    }
+
+    private synchronized ToNativeConverter lookupAndCacheToNativeConverter(Class klazz, ToNativeContext context) {
+        Map<Collection<Annotation>, ToNativeConverter> converterMap = toNativeConverterMap.get(klazz);
+        ToNativeConverter toNativeConverter = converterMap != null ? converterMap.get(context.getAnnotations()) : null;
+        if (toNativeConverter == null) {
+            Map<Collection<Annotation>, ToNativeConverter> cm = new HashMap<Collection<Annotation>, ToNativeConverter>((converterMap != null ? converterMap.size() : 0) + 1, 0.5f);
+            if (converterMap != null) cm.putAll(converterMap);
+            cm.put(context.getAnnotations(), toNativeConverter = mapper.getToNativeConverter(klazz, context));
+
+            Map<Class, Map<Collection<Annotation>, ToNativeConverter>> m
+                    = new IdentityHashMap<Class, Map<Collection<Annotation>, ToNativeConverter>>(toNativeConverterMap.size() + 1);
+            m.putAll(toNativeConverterMap);
+            m.put(klazz, Collections.unmodifiableMap(cm));
+            toNativeConverterMap = Collections.unmodifiableMap(m);
+        }
+
+        return toNativeConverter;
+    }
+}
