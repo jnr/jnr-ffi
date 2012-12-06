54c2490e40deb27ff66beb30b21eb64a6ee35123
Wayne Meissner <wmeissner@gmail.com>
2012-12-06 18:31:48 +1100

    Use StringResultConverter for closure parameters

--- src/main/java/jnr/ffi/provider/jffi/ClosureTypeMapper.java
+++ src/main/java/jnr/ffi/provider/jffi/ClosureTypeMapper.java
@@ -4,11 +4,17 @@ import jnr.ffi.Struct;
 import jnr.ffi.mapper.*;
 import jnr.ffi.provider.converters.EnumConverter;
 import jnr.ffi.provider.ParameterFlags;
+import jnr.ffi.provider.converters.StringResultConverter;
+
+import java.nio.charset.Charset;
 
 final class ClosureTypeMapper implements TypeMapper {
     public FromNativeConverter getFromNativeConverter(Class type, FromNativeContext context) {
         if (Enum.class.isAssignableFrom(type)) {
             return EnumConverter.getInstance(type.asSubclass(Enum.class));
+
+        } else if (CharSequence.class.isAssignableFrom(type)) {
+            return StringResultConverter.getInstance(Charset.defaultCharset());
 
         } else {
             return null;
