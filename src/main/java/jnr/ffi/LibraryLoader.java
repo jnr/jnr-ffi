b9fccddf58d9a751fa9227b1495ed1d251e43fdd
Christian Meier <m.kristian@web.de>
2015-08-06 18:47:41 +0200

    better search path for native libraries on ubuntu
    using the tar archive of oracle jdk on ubuntu does not
    setup the load library path for finding libcrypt (used by jnr-posix).
    this results in failed native support for jnr-posix. installing the
    jdk through some debian/ubuntu repositories do not show this problem.
    
    this patch also looks at /etc/ld.so.conf and /etc/ld.so.conf.d/* to
    setup the internal search path. with this the above combination of
    oracle jdk and ubuntu does find the native libraries for jnr-posix.
    
    fixes https://github.com/jruby/jruby/issues/2913
    and https://github.com/jruby/jruby/issues/3145
    probably also https://github.com/elastic/logstash/issues/3127#issuecomment-101068714
    
    Sponsored by Lookout Inc.

--- src/main/java/jnr/ffi/LibraryLoader.java
+++ src/main/java/jnr/ffi/LibraryLoader.java
@@ -23,7 +23,10 @@ import jnr.ffi.provider.FFIProvider;
 import jnr.ffi.provider.LoadedLibrary;
 import jnr.ffi.provider.NativeInvocationHandler;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -332,6 +335,30 @@ public abstract class LibraryLoader<T> {
 
     private static final class StaticDataHolder {
         private static final List<String> USER_LIBRARY_PATH;
+        private static void addPaths(List<String> paths, File file) {
+            if (!file.isFile() || !file.exists()) return;
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new FileReader(file));
+                String line = in.readLine();
+                while( line != null ) {
+                    if (new File(line).exists()) {
+                        System.err.println(line);
+                        paths.add(line);
+                    }
+                    line = in.readLine();
+                }
+            }
+            catch(IOException ignored) {
+            }
+            finally {
+                if (in != null) {
+                    try { in.close(); }
+                    catch(IOException ignored) {}
+                }
+            }
+        }
+
         static {
             List<String> paths = new ArrayList<String>();
             try {
@@ -341,6 +368,16 @@ public abstract class LibraryLoader<T> {
                 paths.addAll(getPropertyPaths("jna.library.path"));
                 paths.addAll(getPropertyPaths("java.library.path"));
             } catch (Exception ignored) {}
+
+            // only for oracle jdk on ubuntu parse /etc/ld.so.conf and /etc/ld.so.conf.d/*
+            // more details:
+            // https://github.com/jruby/jruby/issues/2913
+            // https://github.com/jruby/jruby/issues/3145
+            // https://github.com/elastic/logstash/issues/3127#issuecomment-101068714
+            addPaths(paths, new File("/etc/ld.so.conf"));
+            for(File file: new File("/etc/ld.so.conf.d").listFiles()) {
+                addPaths(paths, file);
+            }
             USER_LIBRARY_PATH = Collections.unmodifiableList(new ArrayList<String>(paths));
         }
     }
