614ff75f5c3668b085f118f035d515579dc36901
Charles Oliver Nutter <headius@headius.com>
2016-09-22 14:50:49 -0500

    Add a test for #66.
    The test will be skipped on Java 7, but on Java 8 it will ensure
    that the function mapper does not try to map over top of any
    default methods in the interface.

---
+++ src/test/java/jnr/ffi/provider/InterfaceScannerTest.java
@@ -0,0 +1,36 @@
+package jnr.ffi.provider;
+
+import jnr.ffi.TstUtil;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+public class InterfaceScannerTest {
+    private static final Method SPLITERATOR;
+
+    static {
+        Method s = null;
+        try {
+            Collection.class.getMethod("spliterator");
+        } catch (Exception e) {
+            // leave null, tests will be skipped
+        }
+        SPLITERATOR = s;
+    }
+
+    @Test public void loadTestLib() throws Exception {
+        // If we're on Java 8+, proceed
+        Assume.assumeNotNull(SPLITERATOR);
+
+        Collection lib = TstUtil.loadTestLib(Collection.class);
+
+        // spliterator function does not exist in the test lib, so this only works if we're skipping default methods
+        Object spliterator = SPLITERATOR.invoke(lib);
+
+        Assert.assertNotNull(spliterator);
+        Assert.assertEquals("java.util.Spliterator", spliterator.getClass().getName());
+    }
+}
